5931e2e9593df036b253fec97f921d35e9911fbc
YugShrivastava <shrivastavayug04@gmail.com>
2024-10-13 14:16:30 +0530

    All functionalities added, changes made.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,57 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./assets/components/Navbar";
 import { v4 as uuidv4, v4 } from "uuid";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 
 const App = () => {
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
+    const [showFinished, setshowFinished] = useState(true);
 
-    const handleEdit = () => {};
+    const toggleFinished = () => {
+        setshowFinished(!showFinished);
+        console.log(showFinished);
+    };
 
-    const handleDelete = () => {};
+    useEffect(() => {
+        let todosString = localStorage.getItem("todos");
+        if (todosString) {
+            let todos = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todos);
+        }
+    }, []);
+
+    const saveToLs = () => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+    const handleEdit = (e, id) => {
+        let t = todos.filter((item) => {
+            return item.id === id;
+        });
+        setTodo(t[0].todo);
+
+        let newTodos = todos.filter((item) => {
+            return item.id !== id;
+        });
+        setTodos(newTodos);
+        saveToLs();
+    };
+
+    const handleDelete = (e, id) => {
+        let newTodos = todos.filter((item) => {
+            return item.id !== id;
+        });
+        setTodos(newTodos);
+        saveToLs();
+    };
 
     const handleAdd = () => {
         setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
         setTodo("");
-        console.log();
+        saveToLs();
     };
 
     const handleChange = (e) => {
@@ -29,64 +66,92 @@ const App = () => {
         let newTodos = [...todos];
         newTodos[index].isCompleted = !newTodos[index].isCompleted;
         setTodos(newTodos);
+        saveToLs();
     };
 
     return (
         <>
             <Navbar />
-            <div className="container mx-auto bg-violet-100 my-5 rounded-xl p-5 min-h-[80vh]">
-                <div className="addTodo my-5">
-                    <h2 className="text-lg font-bold">Add a ToDo</h2>
+            <div className="mx-3 md:container md:mx-auto bg-violet-100 my-5 rounded-xl p-5 min-h-[80vh] md:w-1/2">
+                <h1 className="text-center font-bold text-2xl py-3">
+                    iTask - Manage todos at one place
+                </h1>
+                <h2 className="text-lg font-bold">Add a ToDo</h2>
+                <div className="addTodo my-5 flex justify-space-between gap-5">
                     <input
                         onChange={handleChange}
                         value={todo}
                         type="text"
-                        className="w-80"
+                        className="w-3/4 px-5 py-1 rounded-lg "
                     />
                     <button
                         onClick={handleAdd}
-                        className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+                        disabled={todo.length <= 3}
+                        className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white disabled:bg-violet-500 transition-all rounded-lg w-1/4"
                     >
-                        Add
+                        Save
                     </button>
                 </div>
+                <input
+                    className="my-3"
+                    onChange={toggleFinished}
+                    type="checkbox"
+                    checked={showFinished}
+                />{" "}
+                <label className="mx-2" htmlFor="show">
+                    Show Finished
+                </label>
+                <div className="h-[1px] bg-black opacity-20 w-[95%] mx-auto my-3"></div>
                 <h2 className="text-lg font-bold">Your Todos</h2>
                 <div className="todos">
+                    {todos.length === 0 && (
+                        <div className="m-5">No Todos to display</div>
+                    )}
                     {todos.map((item) => {
                         return (
-                            <div
-                                key={item.id}
-                                className="todo w-1/4 my-3 flex justify-between"
-                            >
-                                <input
-                                    onChange={handleCheckbox}
-                                    type="checkbox"
-                                    value={item.isCompleted}
-                                    name={item.id}
-                                    id=""
-                                />
+                            (showFinished || !item.isCompleted) && (
                                 <div
-                                    className={
-                                        item.isCompleted ? "line-through" : ""
-                                    }
+                                    key={item.id}
+                                    className="todo w-full my-3 flex justify-between"
                                 >
-                                    {item.todo}
+                                    <div className="flex gap-5">
+                                        <input
+                                            onChange={handleCheckbox}
+                                            type="checkbox"
+                                            checked={item.isCompleted}
+                                            name={item.id}
+                                            id=""
+                                        />
+                                        <div
+                                            className={
+                                                item.isCompleted
+                                                    ? "line-through"
+                                                    : ""
+                                            }
+                                        >
+                                            {item.todo}
+                                        </div>
+                                    </div>
+                                    <div className="buttons flex h-full">
+                                        <button
+                                            onClick={(e) =>
+                                                handleEdit(e, item.id)
+                                            }
+                                            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                                        >
+                                            <FaEdit />
+                                        </button>
+                                        <button
+                                            onClick={(e) =>
+                                                handleDelete(e, item.id)
+                                            }
+                                            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                                        >
+                                            <AiFillDelete />
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className="buttons">
-                                    <button
-                                        onClick={handleEdit}
-                                        className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                                    >
-                                        Edit
-                                    </button>
-                                    <button
-                                        onClick={handleDelete}
-                                        className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
+                            )
                         );
                     })}
                 </div>
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <>
-            <nav className="flex justify-between bg-indigo-900 text-white py-3">
+            <nav className="flex justify-around bg-indigo-900 text-white py-3">
                 <div className="logo">
                     <span className="font-bold text-xl mx-9">iTask</span>
                 </div>
